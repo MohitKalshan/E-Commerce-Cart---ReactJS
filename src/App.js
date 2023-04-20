72127678954ae18fb6178ebe0fe2a11dc3c3310b
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-04-20 13:46:35 +0530

    add, update and delete product functionality added

--- src/App.js
+++ src/App.js
@@ -9,32 +9,67 @@ class App extends React.Component {
     super();
     this.state = {
       products: [],
-      loading: true
+      loading: true,
     };
+    // this.db = db.firestore();
   }
 
   componentDidMount() {
-      db.collection("products").get().then(querySnapshot => {
-        const products = querySnapshot.docs.map(doc => {
-          const data = doc.data()
-          data['id'] = doc.id;
-          return data;
-        });
-        this.setState({ products: products, loading: false });
+    // db.collection("products").get().then(querySnapshot => {
+    //   const products = querySnapshot.docs.map(doc => {
+    //     const data = doc.data()
+    //     data['id'] = doc.id;
+    //     return data;
+    //   });
+    //   this.setState({ products: products, loading: false });
+    // });
+    db.collection("products").onSnapshot((querySnapshot) => {
+      const products = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
       });
+      this.setState({ products: products, loading: false });
+    });
   }
+
+  addProduct = () => {
+    db.collection()
+      .add({
+        img: "",
+        price: 99,
+        qty: 3,
+        title: "Washing Machine",
+      })
+      .then((docRef) => {
+        console.log("Product added", docRef);
+      })
+      .catch((err) => {
+        console.log("Error adding product", err);
+      });
+  };
 
   handleIncreaseQuantity = (product) => {
     // console.log(product);
     const { products } = this.state;
     const index = products.indexOf(product);
-    products[index].qty += 1;
-
-    this.setState({
-      //  products : products, OR shorthand
-      products,
+    // products[index].qty += 1;
+    // this.setState({
+    //   //  products : products, OR shorthand
+    //   products,
+    // });
+    const docRef = db.collection("products").doc(products[index].id);
+    docRef.update({
+      qty: products[index].qty + 1,
+    })
+    .then(() => {
+      console.log("Updated successfully");
+    })
+    .catch(err => {
+      console.log("Error updating product",err);
     });
   };
+
   handleDecreaseQuantity = (product) => {
     // console.log(product);
     const { products } = this.state;
@@ -43,18 +78,37 @@ class App extends React.Component {
     if (products[index].qty === 0) {
       return;
     }
-    products[index].qty -= 1;
+    // products[index].qty -= 1;
 
-    this.setState({
-      //  products : products, OR shorthand
-      products,
+    // this.setState({
+    //   //  products : products, OR shorthand
+    //   products,
+    // });
+    const docRef = db.collection("products").doc(products[index].id);
+    docRef.update({
+      qty: products[index].qty - 1,
+    })
+    .then(() => {
+      console.log("Updated successfully");
+    })
+    .catch(err => {
+      console.log("Error updating product",err);
     });
   };
+
   handleDeleteProduct = (id) => {
-    const { products } = this.state;
-    const item = products.filter((item) => item.id !== id); // filter returns a new array
-    this.setState({
-      products: item,
+    // const { products } = this.state;
+    // const item = products.filter((item) => item.id !== id); // filter returns a new array
+    // this.setState({
+    //   products: item,
+    // });
+    const docRef = db.collection("products").doc(id);
+    docRef.delete()
+    .then(() => {
+      console.log("Deleted successfully");
+    })
+    .catch(err => {
+      console.log("Error updating product",err);
     });
   };
 
@@ -78,11 +132,13 @@ class App extends React.Component {
     return cartTotal;
   };
 
+
   render() {
     const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
+        <button onClick={this.addProduct}>Add Product</button>
         <Cart
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
